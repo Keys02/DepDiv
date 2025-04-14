9a5fd174c20d8b376e3929343acb2f0f48c5e6a0
Keys <keys14052002@gmail.com>
2025-04-14 23:01:55 +0000

    Fix search question bug

--- controllers/search-qtn.php
+++ controllers/search-qtn.php
@@ -1,6 +1,8 @@
 <?php
     require_once "models/Question.php";
     $question = new Question($database);
+    $logged_in_user_id = $user_login_session->getLoggedInUser();
+    
     if($_GET['search-query'] !== '') {
         $search_query = $_GET['search-query'];;
         $questions_from_db = $question->searchQuestion($search_query);
