a4175b0776c6ed386716a7888babed1a78e4ef39
Keys <keys14052002@gmail.com>
2025-04-25 15:13:54 +0000

    Make the year at footer generate the current year always

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,11 +2,13 @@
 /*
     Partial: Will be included in other views before they are rendered, not directly rendered.
 */
+    $year = date('Y');
+
     $footer = 
                 "
                                 </section>
                                 <footer>
-                                    <p><small>&copy;</small> DepDiv 2025. All rights reserved </p>
+                                    <p><small>&copy;</small> DepDiv {$year}. All rights reserved </p>
                                     <p class='author'>Keys🚀</p>
                                 </footer>
                                 <script src='assets/js/jquery-3.7.1.min.js'></script>
